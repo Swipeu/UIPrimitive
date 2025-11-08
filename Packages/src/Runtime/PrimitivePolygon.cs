d4d849cb3eeba8bb0481b1e2ee74ddd8a265ea23
Dan <dan.erikson2@gmail.com>
2025-11-08 21:36:45 +0100

    removed namespaces

--- Packages/src/Runtime/PrimitivePolygon.cs
+++ Packages/src/Runtime/PrimitivePolygon.cs
@@ -1,10 +1,5 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Drawing;
-using System.Linq;
-using Unity.VisualScripting;
-using UnityEditor;
 using UnityEngine;
 
 namespace Swipeu.UIPrimitive
